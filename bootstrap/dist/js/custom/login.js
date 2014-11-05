bc65b55ee777faecde0ffb901eb505c98de69a79
Enea Tsanai <fotisdmc@hotmail.com>
2014-11-05 03:18:03 +0200

    Waiter login ajax

--- bootstrap/dist/js/custom/login.js
+++ bootstrap/dist/js/custom/login.js
@@ -1,17 +1,18 @@
-$('#login_form').submit(function() {
-    //$.mobile.showPageLoadingMsg();
+// Manager
+$('#m_login-form').submit(function() {
     $.ajax({
-        url: "/web_based_ordering_system/project/waiter_website/layers/logs/waiter_login.php?action=login",
+        url: "/wbos/manager/layers/logs/waiter_login.php?action=login",
         type: "POST",
         dataType: "json",
-        data: $("form#login_form").serialize(),
-        beforeSend: function() { $.mobile.showPageLoadingMsg(); }, // Show spinner
-        complete: function() { $.mobile.hidePageLoadingMsg() }, // Hide spinner
+        data: $("form#m_login-form").serialize(),
+        // beforeSend: function() { $loading.show(); }, // Show spinner
+        // complete: function() { $loading.hide()() }, // Hide spinner
         success: function( response ) {
             if(response["Result"] == "OK") {
                 loggedWaiter["username"] = response["username"];
                 loggedWaiter["uid"] = response["uid"];
                 page = "user_panel";
+                alert("Something went wrong");
                 displayPageContent( page );
             }
             else if (response["Result"] == "ERROR") alert("Λάθος όνομα χρήστη ή κωδικός");
@@ -21,7 +22,37 @@ $('#login_form').submit(function() {
             if(jqXHR.status&&jqXHR.status==400){
                 alert(jqXHR.responseText); 
             } else {
-                // alert("Something went wrong");
+                alert("Something went wrong");
+            }
+        }
+    });
+    return false;
+});
+// Waiter
+$('#w_login-form').submit(function() {
+    $.ajax({
+        url: "/wbos/waiter/layers/logs/waiter_login.php?action=login",
+        type: "POST",
+        dataType: "json",
+        data: $("form#w_login-form").serialize(),
+        // beforeSend: function() { $loading.show(); }, // Show spinner
+        // complete: function() { $loading.hide()() }, // Hide spinner
+        success: function( response ) {
+            if(response["Result"] == "OK") {
+                loggedWaiter["username"] = response["username"];
+                loggedWaiter["uid"] = response["uid"];
+                page = "user_panel";
+                alert("Something went wrong");
+                displayPageContent( page );
+            }
+            else if (response["Result"] == "ERROR") alert("Λάθος όνομα χρήστη ή κωδικός");
+            else alert("ERROR");
+        },
+        error: function(jqXHR,error, errorThrown) {  
+            if(jqXHR.status&&jqXHR.status==400){
+                alert(jqXHR.responseText); 
+            } else {
+                alert("Something went wrong");
             }
         }
     });
